ea3455392ab1a0479b44858dd494931da33c452f
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-12-22 21:52:49 +0300

    registration Works

--- app/src/main/java/com/bootcamp/stations/ui/RegisterFragment.kt
+++ app/src/main/java/com/bootcamp/stations/ui/RegisterFragment.kt
@@ -10,18 +10,30 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 
 import com.bootcamp.stations.R
+import com.bootcamp.stations.Test
 import com.bootcamp.stations.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
-    private var _binding : FragmentRegisterBinding? = null
+    private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding
     private val viewModel: UserViewModel by activityViewModels()
+
+
+//   private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//       auth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
 //        binding?.lifecycleOwner = this
         return _binding?.root
     }
@@ -35,21 +47,41 @@ class RegisterFragment : Fragment() {
         }
         binding?.btnSingupReg?.setOnClickListener {
             checkUSerName()
+            testTest()
         }
 //        binding?.regUsername?.setOnTextChanged { text, start, before, count ->  }
+
     }
 
-     private fun checkUSerName(){
-          val username = binding!!.regUsername
-          val usernamelay = binding!!.regUsernameLay
-          val pass = binding!!.regPassword
-          val passlay = binding!!.regPasswordLay
-          val rePass = binding!!.regCnfPassword
-          val rePasslay = binding!!.regCnfPasswordLay
-        val icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_baseline_error_outline_24)
-        icon!!.setBounds(0,0,icon.intrinsicWidth,icon.intrinsicHeight)
-       viewModel.checkUserInfoEmpty(username,usernamelay, icon)
-       viewModel.checkUserPassEmpty(pass,passlay,icon)
-       viewModel.checkUserRePassEmpty(rePass,rePasslay,icon)
+    private fun checkUSerName() {
+        val username = binding!!.regUsername
+        val usernamelay = binding!!.regUsernameLay
+        val pass = binding!!.regPassword
+        val passlay = binding!!.regPasswordLay
+        val rePass = binding!!.regCnfPassword
+        val rePasslay = binding!!.regCnfPasswordLay
+        val icon = AppCompatResources.getDrawable(
+            requireContext(),
+            R.drawable.ic_baseline_error_outline_24
+        )
+        icon!!.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+
+        viewModel.checkUserInfoEmpty(username, usernamelay, icon)
+        viewModel.checkUserPassEmpty(pass, passlay, icon)
+        viewModel.checkUserRePassEmpty(rePass, rePasslay, icon)
+
+
     }
-}
+
+    private fun testTest () {
+
+        val username = binding!!.regUsername.text.toString()
+        val pass = binding!!.regPassword.text.toString()
+
+
+
+        Test().createAccount(username,pass)
+
+    }
+
+} // end fragment
