3c5ee8d2fa592b16c2cfec2a81c0f953411683ab
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2022-01-22 01:58:48 +0300

    fixed the respond on the permission request

--- app/src/main/java/com/bootcamp/stations/homeMap/ui/HomeFragment.kt
+++ app/src/main/java/com/bootcamp/stations/homeMap/ui/HomeFragment.kt
@@ -49,10 +49,7 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
     //entry point to the Fused Location Provider.
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    private val personIcon: BitmapDescriptor by lazy {
-        val color = ContextCompat.getColor(requireContext(), R.color.Cyan_700)
-        BitmapHelper.vectorToBitmap(requireContext(), R.drawable.ic_profile, color)
-    }
+
     private var locationPermissionGranted = false
     private var lastKnownLocation: Location? = null
 
@@ -76,9 +73,14 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         Places.initialize(requireContext(), BuildConfig.GOOGLE_MAPS_API_KEY)
 
-        placesClient = Places.createClient(requireContext())
+        //region the map fragment that shows the map
+
+        placesClient = Places.createClient(requireActivity())
+
+
 
         if (savedInstanceState != null) {
             lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
@@ -95,21 +97,17 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+
         // Set up the toolbar.
         (activity as? AppCompatActivity)?.setSupportActionBar(binding?.appBarSetting)
 
-        // Construct a FusedLocationProviderClient.
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(this.requireContext())
 
-        //region the map fragment that shows the map
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.myMap) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-        //endregion
+
 
         return binding?.root
     }
+
+
 
 
 //region inflate the menu with the tool bar
@@ -147,6 +145,16 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.myMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        //endregion
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this.requireActivity())
+
+
     }
 
     //region save the map instance location
@@ -160,6 +168,7 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
     //endregion
 
     //region checks whether the user has granted fine location permission. If not, it requests the permission
+    @SuppressLint("MissingPermission")
     private fun getLocationPermission() {
         /*
          * Request location permission, so that we can get the location of the
@@ -168,40 +177,45 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
          */
         if (ContextCompat.checkSelfPermission(
                 this.requireContext().applicationContext,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            locationPermissionGranted = true
-//            updateLocationUI()
-//            getDeviceLocation()
-        } else {
-            ActivityCompat.requestPermissions(
-                this.requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        locationPermissionGranted = false
-        when (requestCode) {
-            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
-                    locationPermissionGranted = true
-//                    updateLocationUI()
-                }
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this.requireActivity(),
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+            return }
+        locationPermissionGranted = true
+        updateLocationUI()
+        map?.isMyLocationEnabled= true
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null){
+                lastKnownLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map?.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,14f))
             }
         }
+
     }
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        locationPermissionGranted = false
+//        when (requestCode) {
+//            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+//
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.isNotEmpty() &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+//                ) {
+//                    locationPermissionGranted = true
+//                    updateLocationUI()
+//                }
+//            }
+//        }
+//    }
 
     //endregion
 
@@ -222,8 +236,8 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
                 getLocationPermission()
             }
         } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message, e)
-        }
+
+    }
     }
 //endregion
 
@@ -248,8 +262,7 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
                             )
                         }
                     } else {
-                        Log.d(TAG, "Current location is null. Using defaults.")
-                        Log.e(TAG, "Exception: %s", task.exception)
+
                         map?.moveCamera(
                             CameraUpdateFactory
                                 .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat())
@@ -277,8 +290,37 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
             val request = FindCurrentPlaceRequest.newInstance(placeFields)
             // Get the likely places - that is, the businesses and other points of interest that
             // are the best match for the device's current location.
-//            val placeResult = placesClient.findCurrentPlace(request)
-//
+            val placeResult = placesClient.findCurrentPlace(request)
+            placeResult.addOnCompleteListener { task ->
+                if (task.isSuccessful && task.result != null) {
+                    val likelyPlaces = task.result
+                    map!!.isMyLocationEnabled = true
+                    val count =
+                        if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
+                            likelyPlaces.placeLikelihoods.size
+                        } else {
+                            M_MAX_ENTRIES
+                        }
+                    var i = 0
+                    likelyPlaceNames = arrayOfNulls(count)
+                    likelyPlaceAddresses = arrayOfNulls(count)
+                    likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
+                    likelyPlaceLatLngs = arrayOfNulls(count)
+                    for (placeLikelihood in likelyPlaces.placeLikelihoods ?: emptyList()) {
+                        likelyPlaceNames[i] = placeLikelihood.place.name
+                        likelyPlaceAddresses[i] = placeLikelihood.place.address
+                        likelyPlaceAttributions[i] = placeLikelihood.place.attributions
+                        likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
+                        i++
+                        if (i > count - 1) {
+                            break
+                        }
+                    }
+                    openPlacesDialog()
+                } else {
+                    Log.e(TAG, "Exception: %s", task.exception)
+                }
+            }
         } else {
             defaultMarkerOfTheUSer()
         }
@@ -288,7 +330,7 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
     //region default user location
     private fun defaultMarkerOfTheUSer() {
         // The user has not granted permission.
-        Log.i(TAG, "The user did not grant location permission.")
+
         map?.addMarker(
             MarkerOptions()
                 .title((R.string.default_info_title.toString()))
@@ -296,6 +338,7 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
                 .position(defaultLocation)
                 .snippet((R.string.default_info_snippet.toString()))
         )
+
     }
     //endregion
 
@@ -343,9 +386,11 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
     //region on map ready operations
     override fun onMapReady(googleMap: GoogleMap) {
         this.map = googleMap
-//        getLocationPermission()
+        map!!.uiSettings.isZoomControlsEnabled = true
+
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                getLocationPermission()
                 viewModel.mapUiState.collect {
                     addPolyLine(googleMap, it.marker)
                 }
@@ -354,16 +399,16 @@ internal class HomeFragment : Fragment(), OnMapReadyCallback {
         navigation(googleMap)
 
         binding?.navigationButton?.setOnClickListener {
-            if (locationPermissionGranted)
+            if (locationPermissionGranted && lastKnownLocation != null)
             navigateOnClick(googleMap)
         }
-        getLocationPermission()
+//        getLocationPermission()
 
         // Turn on the My Location layer and the related control on the map.
-        updateLocationUI()
+//        updateLocationUI()
 
         // Get the current location of the device and set the position of the map.
-        getDeviceLocation()
+//        getDeviceLocation()
 
         map!!.setOnMarkerClickListener {
 
